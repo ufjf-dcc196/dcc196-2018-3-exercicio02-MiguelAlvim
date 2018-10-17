85f8131f046ae902d42fc71b5f5fd9e89b286167
MiguelAlvim <miguelalvim_2@hotmail.com>
2018-10-17 13:36:51 -0300

    capacidade de edicao adicionada

--- app/src/main/java/miguel/ex2/MainActivity.java
+++ app/src/main/java/miguel/ex2/MainActivity.java
@@ -6,24 +6,26 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
-    Button bttAdd;
-    EditText txtAdd;
+    Button bttAdd,bttEdit;
+    EditText txtAdd,txtEdit;
     ListView list;
 
-    LinkedList<String> linkedList = new LinkedList<String>();
+    LinkedList<String> linkedListNames = new LinkedList<String>();
+    LinkedList<Long> linkedListIds = new LinkedList<Long>();
     ArrayAdapter<String> arrayAdapter;
 
     DBHandler bdHandler;
     SQLiteDatabase bd;
+    int listPosition=-1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,33 +34,63 @@ public class MainActivity extends AppCompatActivity {
 
         bttAdd = findViewById(R.id.bttAdd);
         txtAdd = findViewById(R.id.txtAdd);
+        bttEdit = findViewById(R.id.bttEdit);
+        txtEdit = findViewById(R.id.txtEdit);
         list   = findViewById(R.id.list);
 
-        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,linkedList);
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1, linkedListNames);
         list.setAdapter(arrayAdapter);
 
         bdHandler = new DBHandler(getApplicationContext());
         bd = bdHandler.getReadableDatabase();
 
-        String[] cols = {"id","name"};
-        Cursor c = bd.query("series", cols, null, null, null,null , null);
+        updateListWithDB();
 
-        while (c.moveToNext()){
-            linkedList.add(c.getString(1));
-            arrayAdapter.notifyDataSetChanged();
-        }
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                txtEdit.setText(list.getItemAtPosition(position)+"");
+                listPosition = position;
+            }
+        });
 
         bttAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                linkedList.add(txtAdd.getText().toString());
                 ContentValues vals = new ContentValues();
                 vals.put("name",txtAdd.getText().toString());
-                if(bd.insert("series", null, vals)>-1) {
+                long id = bd.insert("series", null, vals);
+                if(id>-1) {
+                    linkedListNames.add(txtAdd.getText().toString());
+                    linkedListIds.add(id);
                     arrayAdapter.notifyDataSetChanged();
                 }
             }
         });
+        bttEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues vals = new ContentValues();
+                vals.put("name",txtEdit.getText().toString());
+                String[] args = {linkedListIds.get(listPosition)+""};
+                long id = bd.update("series", vals, "id = ?", args);
+                if(id>0){
+                    linkedListNames.set(listPosition,txtEdit.getText().toString());
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+    }
+    private void updateListWithDB(){
+        String[] cols = {"id","name"};
+        Cursor c = bd.query("series", cols, null, null, null,null , null);
+        linkedListNames.clear();
+        linkedListIds.clear();
 
+        while (c.moveToNext()){
+            linkedListNames.add(c.getString(1));
+            linkedListIds.add(c.getLong(0));
+            arrayAdapter.notifyDataSetChanged();
+        }
     }
 }
